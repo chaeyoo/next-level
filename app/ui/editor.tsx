2a5960e5924fa656232832e0fcaa85a2c191cc4b
chaeyoo <chaeh0124@gmail.com>
2024-08-27 22:40:04 +0900

    [FEAT] 에디터로 변경

---
+++ app/ui/editor.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Editor } from "@tinymce/tinymce-react";
+import { MutableRefObject, useRef } from "react";
+import type { Editor as TinyMCEEditor } from "tinymce";
+
+export default function EditorComponent({
+	setContent,
+	initContent,
+}: {
+	setContent: (content: string) => void;
+	initContent: string;
+}) {
+	const handleEditorChange = (content: string) => {
+		setContent(content);
+	};
+	const editorRef = useRef<TinyMCEEditor | null>(null);
+	return (
+		<Editor
+			apiKey="y0f8mrjec8xssg8xszefhpq5y57p2zokazb85hdyqiraw5qm"
+			onInit={(evt, editor) => (editorRef.current = editor)}
+			initialValue={initContent}
+			init={{
+				height: 500,
+				menubar: false,
+				plugins: [
+					"advlist",
+					"autolink",
+					"lists",
+					"link",
+					"image",
+					"charmap",
+					"preview",
+					"anchor",
+					"searchreplace",
+					"visualblocks",
+					"code",
+					"fullscreen",
+					"insertdatetime",
+					"media",
+					"table",
+					"code",
+					"help",
+					"wordcount",
+				],
+				toolbar:
+					"undo redo | blocks | " +
+					"bold italic forecolor | alignleft aligncenter " +
+					"alignright alignjustify | bullist numlist outdent indent | " +
+					"removeformat | help",
+				content_style:
+					"body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+			}}
+			onEditorChange={handleEditorChange}
+		/>
+	);
+}
--- app/ui/post/edit-form.tsx
+++ app/ui/post/edit-form.tsx
@@ -4,123 +4,131 @@ import { CategoryField } from "@/app/lib/definitions";
 import { PencilSquareIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { useActionState } from "react";
+import { useActionState, useRef, useState } from "react";
 import { PostsTable } from "@/app/lib/posts/definitions";
 import { State, updatePost } from "@/app/lib/posts/actions";
+import { Editor } from "@tinymce/tinymce-react";
+import type { Editor as TinyMCEEditor } from "tinymce";
+import EditorComponent from "../editor";
 
 export default function EditPosteForm({
-  post,
-  categories,
+	post,
+	categories,
 }: {
-  post: PostsTable;
-  categories: CategoryField[];
+	post: PostsTable;
+	categories: CategoryField[];
 }) {
-  const initialState: State = { message: null, errors: {} };
-  const updatePostWithId = updatePost.bind(null, post.id);
-  const [state, formAction] = useActionState(updatePostWithId, initialState);
-  return (
-    <form action={formAction}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
-        <div className="mb-4">
-          <label
-            htmlFor="categoryId"
-            className="mb-2 block text-sm font-medium"
-          >
-            카테고리
-          </label>
-          <div className="relative">
-            <select
-              id="categoryId"
-              name="categoryId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={post.category_id}
-            >
-              <option value="" disabled>
-                카테고리 선택
-              </option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-            </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          <div id="categoryId-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.categoryId &&
-              state.errors.categoryId.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>
-        </div>
+	const initialState: State = { message: null, errors: {} };
+	const updatePostWithId = updatePost.bind(null, post.id);
+	const [state, formAction] = useActionState(updatePostWithId, initialState);
+	const [content, setContent] = useState(post.content);
 
-        {/* Invoice Amount */}
-        <div className="mb-4">
-          <label htmlFor="title" className="mb-2 block text-sm font-medium">
-            제목
-          </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id="title"
-                name="title"
-                type="text"
-                placeholder="제목을 입력해주세요."
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                defaultValue={post.title}
-                required
-              />
-              <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-          <div id="title-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.title &&
-              state.errors.title.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>
-        </div>
+	return (
+		<form action={formAction}>
+			<div className="rounded-md bg-gray-50 p-4 md:p-6">
+				<div className="mb-4">
+					<label
+						htmlFor="categoryId"
+						className="mb-2 block text-sm font-medium"
+					>
+						카테고리
+					</label>
+					<div className="relative">
+						<select
+							id="categoryId"
+							name="categoryId"
+							className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+							defaultValue={post.category_id}
+						>
+							<option value="" disabled>
+								카테고리 선택
+							</option>
+							{categories.map((category) => (
+								<option key={category.id} value={category.id}>
+									{category.name}
+								</option>
+							))}
+						</select>
+						<UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+					</div>
+					<div id="categoryId-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.categoryId &&
+							state.errors.categoryId.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
+					</div>
+				</div>
 
-        <div className="mb-4">
-          <label htmlFor="title" className="mb-2 block text-sm font-medium">
-            내용
-          </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <textarea
-                id="content"
-                name="content"
-                defaultValue={post.content}
-                placeholder="내용을 입력해주세요."
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                required
-              />
-            </div>
-          </div>
-          <div id="content-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.content &&
-              state.errors.content.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>
-        </div>
-      </div>
-      <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
-        <Button type="submit">Edit Post</Button>
-      </div>
-    </form>
-  );
+				{/* Invoice Amount */}
+				<div className="mb-4">
+					<label htmlFor="title" className="mb-2 block text-sm font-medium">
+						제목
+					</label>
+					<div className="relative mt-2 rounded-md">
+						<div className="relative">
+							<input
+								id="title"
+								name="title"
+								type="text"
+								placeholder="제목을 입력해주세요."
+								className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+								defaultValue={post.title}
+								required
+							/>
+							<PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+						</div>
+					</div>
+					<div id="title-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.title &&
+							state.errors.title.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
+					</div>
+				</div>
+
+				<div className="mb-4">
+					<label htmlFor="title" className="mb-2 block text-sm font-medium">
+						내용
+					</label>
+					<div className="relative mt-2 rounded-md">
+						<div className="relative">
+							<textarea
+								id="content"
+								name="content"
+								defaultValue={post.content}
+								placeholder="내용을 입력해주세요."
+								className="peer hidden w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+								required
+							/>
+							<EditorComponent
+								setContent={setContent}
+								initContent={post.content}
+							/>
+						</div>
+					</div>
+					<div id="content-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.content &&
+							state.errors.content.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
+					</div>
+				</div>
+			</div>
+			<div className="mt-6 flex justify-end gap-4">
+				<Link
+					href="/dashboard/invoices"
+					className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+				>
+					Cancel
+				</Link>
+				<Button type="submit">Edit Post</Button>
+			</div>
+		</form>
+	);
 }

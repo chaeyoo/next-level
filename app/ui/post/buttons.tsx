77a7001277aeba416f19e45d18cca66da08b3f43
chaeyoo <chaeh0124@gmail.com>
2024-09-02 16:33:38 +0900

    [FEAT] 좋아요 서버 로직 작성 - authorization 고려

--- app/ui/post/buttons.tsx
+++ app/ui/post/buttons.tsx
@@ -1,40 +1,67 @@
-import { deletePost } from "@/app/lib/posts/actions";
+import { deletePost, likePost, unlikePost } from "@/app/lib/posts/actions";
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as HeartSolid } from "@heroicons/react/24/solid";
 export function CreatePost() {
-	return (
-		<Link
-			href="/posts/create"
-			className="flex h-10 items-center rounded-lg bg-slate-500 px-4 text-sm font-medium text-white transition-colors hover:bg-slate-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-500"
-		>
-			<span className="hidden md:block">글쓰기</span>{" "}
-			<PlusIcon className="h-5 md:ml-4" />
-		</Link>
-	);
+  return (
+    <Link
+      href="/posts/create"
+      className="flex h-10 items-center rounded-lg bg-slate-500 px-4 text-sm font-medium text-white transition-colors hover:bg-slate-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-500"
+    >
+      <span className="hidden md:block">글쓰기</span>{" "}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
 }
 
 export function UpdatePost({ id }: { id: string }) {
-	return (
-		<Link
-			href={`/posts/${id}/edit`}
-			className="rounded-md border p-2 hover:bg-gray-100"
-		>
-			<PencilIcon className="w-5" />
-		</Link>
-	);
+  return (
+    <Link
+      href={`/posts/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
+    </Link>
+  );
 }
 
 export function DeletePost({ id }: { id: string }) {
-	const deletePostWithId = deletePost.bind(null, id);
-	return (
-		<>
-			<form action={deletePostWithId}>
-				<button className="rounded-md border p-2 hover:bg-gray-100">
-					<span className="sr-only">Delete</span>
-					<TrashIcon className="w-5" />
-				</button>
-			</form>
-		</>
-	);
+  const deletePostWithId = deletePost.bind(null, id);
+  return (
+    <>
+      <form action={deletePostWithId}>
+        <button className="rounded-md border p-2 hover:bg-gray-100">
+          <span className="sr-only">Delete</span>
+          <TrashIcon className="w-5" />
+        </button>
+      </form>
+    </>
+  );
+}
+
+export function LikePostBtn({ id }: { id: string }) {
+  const likePostWithId = likePost.bind(null, id);
+  return (
+    <>
+      <form action={likePostWithId}>
+        <button className="p-2">
+          <HeartIcon className="w-6 h-6 text-gray-500" />
+        </button>
+      </form>
+    </>
+  );
+}
+
+export function UnlikePostBtn({ id }: { id: string }) {
+  const unlikePostWithId = unlikePost.bind(null, id);
+  return (
+    <>
+      <form action={unlikePostWithId}>
+        <button className="p-2">
+          <HeartSolid className="w-6 h-6 text-red-500" />
+        </button>
+      </form>
+    </>
+  );
 }
--- app/ui/post/like-post.tsx
+++ app/ui/post/like-post.tsx
@@ -1,24 +1,32 @@
 import { PostsTable } from "@/app/lib/posts/definitions";
 import { HeartIcon as HeartSolid } from "@heroicons/react/24/solid";
-import { HeartIcon } from "@heroicons/react/24/outline";
+import { LikePostBtn, UnlikePostBtn } from "./buttons";
+import { auth } from "@/auth";
 export default async function LikePost({ post }: { post: PostsTable }) {
+  const session = await auth();
+  const userId = session?.user.id || undefined;
+  return (
+    <div className="flex items-center space-x-2">
+      <span
+        className={`p-2 rounded-full transition-colors duration-200 ${
+          post.isLiked
+            ? "bg-red-100 hover:bg-red-200"
+            : "bg-gray-100 hover:bg-gray-200"
+        }`}
+      >
+        {userId !== post.user_id && post.isLiked && (
+          <UnlikePostBtn id={post.id} />
+        )}
 
-	return (
-		<div className="flex items-center space-x-2">
-			<button
-				className={`p-2 rounded-full transition-colors duration-200 ${
-					post.isLiked
-						? "bg-red-100 hover:bg-red-200"
-						: "bg-gray-100 hover:bg-gray-200"
-				}`}
-			>
-				{post.isLiked ? (
-					<HeartSolid className="w-6 h-6 text-red-500" />
-				) : (
-					<HeartIcon className="w-6 h-6 text-gray-500" />
-				)}
-			</button>
-			<span className="text-sm font-medium text-gray-700">{post.like_count}</span>
-		</div>
-	);
+        {userId !== post.user_id && !post.isLiked && (
+          <LikePostBtn id={post.id} />
+        )}
+
+        {userId === post.user_id && <HeartSolid className="w-6 h-6 text-slate-500" />}
+      </span>
+      <span className="text-sm font-medium text-gray-700">
+        {post.like_count}
+      </span>
+    </div>
+  );
 }

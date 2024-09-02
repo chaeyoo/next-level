b33be2fef031afb9ca578368150a329e5b5f8e10
chaeyoo <chaeh0124@gmail.com>
2024-09-02 22:51:31 +0900

    [FIX] signup page access

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,45 +1,52 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authOptions = {
-  pages: {
-    signIn: "/login",
-  },
-  session: {
-    strategy: "jwt",
-    maxAge: 24 * 60 * 60,
-    updateAge: 2 * 24 * 60 * 60,
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/");
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/", nextUrl));
-      }
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      if (url.startsWith("/dashboard")) {
-        return `${baseUrl}${url}`;
-      } else if (new URL(url).origin === baseUrl) {
-        return `${baseUrl}`;
-      } else {
-        return baseUrl;
-      }
-    },
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token, user }: any) {
-      if (session.user && token.id) {
-        session.user.id = token.id;
-      }
-      return session;
-    },
-  },
+	pages: {
+		signIn: "/login",
+	},
+	session: {
+		strategy: "jwt",
+		maxAge: 24 * 60 * 60,
+		updateAge: 2 * 24 * 60 * 60,
+	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			const isOnDashboard = nextUrl.pathname.startsWith("/");
+			const isOnLoginPage = nextUrl.pathname === "/login";
+			const isOnSignupPage = nextUrl.pathname === "/signup";
 
-  providers: [], // Add providers with an empty array for now
+			if (isOnLoginPage || isOnSignupPage) {
+				return true;
+			}
+
+			if (isOnDashboard) {
+				if (isLoggedIn) return true;
+				return false; // Redirect unauthenticated users to login page
+			} else if (isLoggedIn) {
+				return Response.redirect(new URL("/", nextUrl));
+			}
+			return true;
+		},
+		async redirect({ url, baseUrl }) {
+			if (url.startsWith("/")) {
+				return `${baseUrl}${url}`;
+			} else if (new URL(url).origin === baseUrl) {
+				return `${baseUrl}`;
+			} else {
+				return baseUrl;
+			}
+		},
+		async jwt({ token, user }) {
+			return { ...token, ...user };
+		},
+		async session({ session, token, user }: any) {
+			if (session.user && token.id) {
+				session.user.id = token.id;
+			}
+			return session;
+		},
+	},
+
+	providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
